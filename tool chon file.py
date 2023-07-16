60d893d20183eadf6b7cf78e7586b843f6aae63e
Khoa Nguyen <xuankhoa.clone1@gmail.com>
2023-07-16 22:08:51 +0700

    update ui and log file

--- tool chon file.py
+++ tool chon file.py
@@ -6,7 +6,7 @@ import re
 import shutil
 import enum
 
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 
 class MyType (enum.Enum):
     Copy = 1
@@ -29,12 +29,10 @@ class MainWindow(QMainWindow):
         self.m_url.setText(fname)
         self.setComboBox()
 
-
     def selectFile(self):
         fileName = self.plainTextEdit.toPlainText()
         m_select = re.findall(r'\d+', fileName)
         return m_select
-
 
     def getFileExtension(self, m_list):
         myset = set()
@@ -45,7 +43,6 @@ class MainWindow(QMainWindow):
                     myset.add(file_extension)
         return list(myset)
 
-
     def getAllFileList(self):
         dir = self.m_url.text()
         if not dir:
@@ -53,7 +50,6 @@ class MainWindow(QMainWindow):
             return []
         fileList = os.listdir(dir)
         return fileList
-
 
     def getFileList(self, m_list):
         m_extension = self.comboBox.currentText()
@@ -100,35 +96,66 @@ class MainWindow(QMainWindow):
         folder_dir = f"{dir} {self.m_newFolder.text()}"
         if not os.path.isdir(folder_dir):
             os.mkdir(folder_dir)
+
+        log_file_path = os.path.join(folder_dir, "log.txt")
+        # print(log_file_path)
+        f = open(log_file_path, "w")
+        f.write("-------------------------Log file-------------------------\n")
+        f.close
+
         m_select = self.selectFile()
         if not m_select:
             self.errLog("Không có file nào được chọn")
             return
         allFile = self.getAllFileList()
         m_list = self.getFileList(allFile)
+        not_copied = []# list để lưu tên The file is not copy or move
+        m_remain = m_select
         count = 0
+        print(len(m_select))
         for i in m_select:
             for j in m_list:
                 if i in j:
                     if type is MyType.Copy:
-                        shutil.copyfile(f"{dir}/{j}", f"{folder_dir}/{j}")
+                        try:
+                            shutil.copyfile(f"{dir}/{j}", f"{folder_dir}/{j}")
+                            count += 1
+                            m_remain.remove(i) # loại bỏ phần tử i ra khỏi m_remain
+                        except:
+                            not_copied.append(j) # thêm tên file vào list not_copied nếu không copy được
+                            f = open(log_file_path, "a")
+                            f.write(j + " Copy lỗi \n") # copy file lỗi
+                            f.close
                     else:
-                        shutil.move(f"{dir}/{j}", f"{folder_dir}/{j}")
-                    count += 1
-                    break
+                        try:
+                            shutil.move(f"{dir}/{j}", f"{folder_dir}/{j}")
+                            count += 1
+                            m_remain.remove(i) # loại bỏ phần tử i ra khỏi m_remain
+                        except:
+                            not_copied.append(j) # thêm tên file vào list not_copied nếu không move được
+                            f = open(log_file_path, "a")
+                            f.write(j +" Di chuyển lỗi \n") # di chuyển file lỗi
+                            f.close
+                    break 
+        if(m_remain):
+            f.write("Các file sau đây không được copy hoặc di chuyển \nNguyên nhân không tìm thấy hoặc bị trùng \n")
+            for i in m_remain:
+                f = open(log_file_path, "a")
+                f.write(i + "\n") 
+                f.close                   
         if count == 0:
             self.errLog(f"Các file được chọn không tồn tại trong {dir}")
         else:
             if type is MyType.Copy:
-                self.infoLog(f"Copy {count} file trong tổng số {len(m_select)} đã chọn \nThư mục: {folder_dir}")
+                self.infoLog(f"Copy {count} file trong tổng số {len(m_select)} đã chọn \nThư mục: {folder_dir} \n Kiểm tra chi tiết trong thư mục log.txt")
             else:
-                self.infoLog(f"Loại bỏ {count} file trong tổng số {len(m_select)} đã chọn \nThư mục chứa file đã loại bỏ: {folder_dir}")
+                self.infoLog(f"Di chuyển {count} file trong tổng số {len(m_select)} đã chọn \nThư mục chứa file đã di chuyển: {folder_dir} \n Kiểm tra chi tiết trong thư mục log.txt" )
 
 app=QApplication(sys.argv)
 mainwindow=MainWindow()
 widget=QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
-widget.setFixedWidth(550)
-widget.setFixedHeight(400)
+widget.setFixedWidth(580)
+widget.setFixedHeight(450)
 widget.show()
 sys.exit(app.exec())
