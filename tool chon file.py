08df07f13083d668b1da29f390a0ea033ef07094
Khoa Nguyen <xuankhoa.clone1@gmail.com>
2023-07-16 16:25:58 +0700

    custom code

--- tool chon file.py
+++ tool chon file.py
@@ -6,7 +6,7 @@ import re
 import shutil
 import enum
 
-VERSION = "1.3.0"
+VERSION = "1.3.1"
 
 class MyType (enum.Enum):
     Copy = 1
@@ -23,115 +23,106 @@ class MainWindow(QMainWindow):
         self.actionInfo.triggered.connect(self.menu)
         self.btn_Move.clicked.connect(self.Move)
 
-#browse đến thư mục chứa ảnh gốc
+    #browse đến thư mục chứa ảnh gốc
     def browsefiles(self):
-        fname=QFileDialog.getExistingDirectory(self)
-        self.m_url.setText(str(fname))
+        fname = QFileDialog.getExistingDirectory(self)
+        self.m_url.setText(fname)
         self.setComboBox()
 
+
     def selectFile(self):
-        fileName = str(self.plainTextEdit.toPlainText())
-        m_select = re.findall(r'\d+', str(fileName))
+        fileName = self.plainTextEdit.toPlainText()
+        m_select = re.findall(r'\d+', fileName)
         return m_select
 
+
     def getFileExtension(self, m_list):
-        myset = []
+        myset = set()
         for i in m_list:
             if "." in i:
-                split_tup = i.split(".")
-                file_extension = split_tup[1]
-            if file_extension not in myset :
-                myset.append(file_extension)
-        return myset
-    
-        #get all file in folder
+                file_extension = i.split(".")[-1]
+                if file_extension not in myset:
+                    myset.add(file_extension)
+        return list(myset)
+
+
     def getAllFileList(self):
         dir = self.m_url.text()
-        if dir == "" :
+        if not dir:
             self.errLog("Vui lòng chọn đường dẫn")
-            return 0
-        # print(dir)
+            return []
         fileList = os.listdir(dir)
         return fileList
-    
-    #get file list with extension
+
+
     def getFileList(self, m_list):
-        mlist = []
         m_extension = self.comboBox.currentText()
-        for i in m_list:
-            if m_extension in i:
-                mlist.append(i)
-        return mlist
+        return [i for i in m_list if m_extension in i]
+
 
     def setComboBox(self):
         allFile = self.getAllFileList()
-        # print("all file = ", allFile)
         mlist = self.getFileExtension(allFile)
-        for a in mlist:
-            self.comboBox.addItem(str(a))
+        self.comboBox.addItems(mlist)
+
 
     def menu(self):
-        dialog = QMessageBox(parent=self, text="Công cụ hỗ trợ lọc file phiên bản " + VERSION + "\nCopyright by Khoa Nguyen")
+        dialog = QMessageBox(parent=self)
+        dialog.setText(f"Công cụ hỗ trợ lọc file phiên bản {VERSION}\nCopyright by Khoa Nguyen")
         dialog.setWindowTitle("Hỗ trợ")
-        ret = dialog.exec()
+        dialog.exec()
+
 
     def errLog(self, m_text):
-        dialog = QMessageBox(parent=self, text= m_text)
+        dialog = QMessageBox(parent=self)
+        dialog.setText(m_text)
         dialog.setWindowTitle("Lỗi")
-        ret = dialog.exec()
+        dialog.exec()
+
 
     def infoLog(self, m_text):
-        dialog = QMessageBox(parent=self, text= m_text)
+        dialog = QMessageBox(parent=self)
+        dialog.setText(m_text)
         dialog.setWindowTitle("Thông tin")
-        ret = dialog.exec()
+        dialog.exec()
 
     def Cancel(self):
         widget.close()
 
     def Copy(self):
         self.filterFile(MyType.Copy)
+
     def Move(self):
         self.filterFile(MyType.Move)
 
     def filterFile(self, type):
-        dir = None
         dir = self.m_url.text()
-        folder_dir = dir + " " + self.m_newFolder.text()
-        isExist = os.path.isdir(folder_dir)
-        if isExist == False:
-            # print("tạo thư mục")
+        folder_dir = f"{dir} {self.m_newFolder.text()}"
+        if not os.path.isdir(folder_dir):
             os.mkdir(folder_dir)
         m_select = self.selectFile()
-        if m_select == []:
+        if not m_select:
             self.errLog("Không có file nào được chọn")
-            return 0
+            return
         allFile = self.getAllFileList()
         m_list = self.getFileList(allFile)
-
-        if type is MyType.Copy:
-            count = 0
-            for i in m_select:
-                for j in m_list:
-                    if i in j:
-                        shutil.copyfile(dir+"/"+j, folder_dir+"/"+j)
-                        count = count + 1
-                        break
-            if count == 0:
-                self.errLog("Các file được chọn không tồn tại trong " + dir)
-            else: 
-                self.infoLog("Copy " + str(count) + " file trong tổng số " + str(len(m_select)) + " đã chọn \n" + "Thư mục: " + folder_dir)
-        elif type is MyType.Move:
-            count = 0
-            for i in m_select:
-                for j in m_list:
-                    if i in j:
-                        shutil.move(dir+"/"+j, folder_dir+"/"+j)
-                        count = count + 1
-                        break
-            if count == 0:
-                self.errLog("Các file được chọn không tồn tại trong " + dir)
-            else: 
-                self.infoLog("Loai bo " + str(count) + " file trong tổng số " + str(len(m_select)) + " đã chọn \n" + "Thư mục chua file da loai bo: " + folder_dir)         
+        count = 0
+        for i in m_select:
+            for j in m_list:
+                if i in j:
+                    if type is MyType.Copy:
+                        shutil.copyfile(f"{dir}/{j}", f"{folder_dir}/{j}")
+                    else:
+                        shutil.move(f"{dir}/{j}", f"{folder_dir}/{j}")
+                    count += 1
+                    break
+        if count == 0:
+            self.errLog(f"Các file được chọn không tồn tại trong {dir}")
+        else:
+            if type is MyType.Copy:
+                self.infoLog(f"Copy {count} file trong tổng số {len(m_select)} đã chọn \nThư mục: {folder_dir}")
+            else:
+                self.infoLog(f"Loại bỏ {count} file trong tổng số {len(m_select)} đã chọn \nThư mục chứa file đã loại bỏ: {folder_dir}")
 
 app=QApplication(sys.argv)
 mainwindow=MainWindow()
